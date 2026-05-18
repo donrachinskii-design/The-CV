5abda9d009983516f77d4ee9f1420c8fa9c20dca
Michael Rachinskii <don.rachinskii@gmail.com>
2026-05-18 17:59:11 +0300

    Initialized React project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Michael Rachinskii</h1>
+        <p>CV</p>
+      </header>
+      <main>
+        <section>
+          <h2>О себе</h2>
+          <p>Добро пожаловать в моё резюме</p>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
